92fd8c8c61460b57bd8976b99ad70cbbc84048a8
ebrahemelsayed5011-art <ebrahemelsayed5011@gmail.com>
2026-08-21 05:22:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 import './src/app.js';
+import { setupBroadcast } from './broadcast.js';
+
+// فحص دوري آمن لالتقاط البوت فور تشغيله وتفعيل الميزة عليه
+let searchBot = setInterval(() => {
+    let currentBot = typeof client !== 'undefined' ? client : (typeof bot !== 'undefined' ? bot : null);
+    if (currentBot) {
+        setupBroadcast(currentBot);
+        clearInterval(searchBot);
+    }
+}, 1000);
+
